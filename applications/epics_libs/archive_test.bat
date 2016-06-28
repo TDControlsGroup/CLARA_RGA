d5cb0f39239b7621b94e062d47ec2e8b020994c4
rorydog1 <rorydog1@gmail.com>
2016-06-28 23:31:52 +0100

    Experimental Python Qt and CA

---
+++ applications/epics_libs/archive_test.bat
@@ -0,0 +1,7 @@
+ set EPICS_HOST_ARCH=win32-x86-mingw
+set PATH=C:\Users\rory\Desktop\base-3.15.3\base-3.15.3\bin\%EPICS_HOST_ARCH%
+set EPICS_BASE=C:\Users\rory\Desktop\base-3.15.3\base-3.15.3
+set PATH=C:\Qtmin\Qt5.5.1\5.5\mingw492_32\bin;C:\Qtmin\Qt5.5.1\Tools\mingw492_32\bin;%PATH%
+set PATH=C:\Users\rory\Desktop\base-3.15.3\base-3.15.3\bin\win32-x86-mingw;%PATH%
+set QWT_INCLUDE_PATH=\Users\rory\Desktop\qwt-6.1.2\src
+softIoc	..\..\..\iocBoot\iocalice\st.cmd
